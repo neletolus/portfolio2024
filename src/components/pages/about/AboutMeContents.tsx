0566eef6f62ad7791e37fb3130be8a6ef15dfe26
yoheisuda <y1405705@gmail.com>
2024-06-26 22:11:29 +0900

    経歴仮追加

--- src/components/pages/about/AboutMeContents.tsx
+++ src/components/pages/about/AboutMeContents.tsx
@@ -22,6 +22,12 @@ export default function AboutMeContents() {
             </div>
             <div className="aboutMeContainer__databody__middle">
                 <h2>経歴：</h2>
+                <AboutMeInfo title={"株式会社Generosity"} titleUrl={"https://generosity.co.jp/"}
+                             date={"2024/06/01 ~ "}
+                             linkUrl={null}
+                             linkText={""} description={<></>}
+                             icons={[logotype.AWS, logotype.HTML, logotype.CSS, logotype.JAVASCRIPT, logotype.THREE]}
+                />
                 <AboutMeInfo title={"株式会社PKSHA Associates"} titleUrl={"https://asc.pkshatech.com/"}
                              date={"2022/1/1 ~ 2024/5/31"}
                              linkUrl={"https://prtimes.jp/main/html/rd/p/000000003.000110483.html"}
@@ -51,8 +57,9 @@ export default function AboutMeContents() {
                                              href="https://developer.dip-net.co.jp/entry/2021/06/11/20%E5%8D%92%E3%83%95%E3%83%AD%E3%83%B3%E3%83%88%E3%82%A8%E3%83%B3%E3%83%89%E3%82%A8%E3%83%B3%E3%82%B8%E3%83%8B%E3%82%A2%E3%81%8C%E3%83%87%E3%82%A3%E3%83%83%E3%83%97%E3%81%A7%E3%81%AE1%E5%B9%B4"
                                              target="_blank">20卒振り返り記事</a>
                                      </li>
-                                     <li><a href="https://developer.dip-net.co.jp/entry/%E3%82%AC%E3%82%BF%E3%81%A4%E3%81%8D%E3%81%AE%E3%81%AA%E3%81%84%E9%AB%98%E3%81%95%E5%8F%AF%E5%A4%89%E3%81%AE%E3%82%AB%E3%83%AB%E3%83%BC%E3%82%BB%E3%83%AB%E3%82%B9%E3%83%A9%E3%82%A4%E3%83%80%E3%83%BC"
-                                        target="_blank">
+                                     <li><a
+                                         href="https://developer.dip-net.co.jp/entry/%E3%82%AC%E3%82%BF%E3%81%A4%E3%81%8D%E3%81%AE%E3%81%AA%E3%81%84%E9%AB%98%E3%81%95%E5%8F%AF%E5%A4%89%E3%81%AE%E3%82%AB%E3%83%AB%E3%83%BC%E3%82%BB%E3%83%AB%E3%82%B9%E3%83%A9%E3%82%A4%E3%83%80%E3%83%BC"
+                                         target="_blank">
                                          複雑なカルーセルスライダーの作成法
                                      </a></li>
                                      <li><a href="https://dippeople.dip-net.jp/13465/" target="_blank">
@@ -66,10 +73,11 @@ export default function AboutMeContents() {
                              linkUrl={null}
                              linkText={""}
                              description={<>
-                                 同社主催の<a href="https://qetic.jp/music/bornnow-180710/290585/" target="_blank">Bornnow 2018</a>にサポートのVJとして参加した縁で、<br />
-                                 インターンのフロントエンドエンジニアとして参加しました。<br />
-                                 <br />
-                                 主に下記案件のマップアプリやタイムテーブル周辺を担当しました。<br />
+                                 同社主催の<a href="https://qetic.jp/music/bornnow-180710/290585/" target="_blank">Bornnow
+                                 2018</a>にサポートのVJとして参加した縁で、<br/>
+                                 インターンのフロントエンドエンジニアとして参加しました。<br/>
+                                 <br/>
+                                 主に下記案件のマップアプリやタイムテーブル周辺を担当しました。<br/>
                                  <a href="https://newskool.jp/casestudy/siw2019/" target="_blank">SIW2019紹介</a>
                              </>}
                              icons={[logotype.WORDPRESS, logotype.HTML, logotype.CSS, logotype.JAVASCRIPT]}/>
@@ -78,8 +86,8 @@ export default function AboutMeContents() {
                              linkUrl={"https://crane-inc.io/posts/interview_suda"}
                              linkText={"卒業時のインタビュー記事"}
                              description={<>
-                                 アルバイトでWebコーダ、フロントエンドエンジニアとして参加しました。<br />
-                                Wordpress案件やECショップの立ち上げ、軽い予約システムの開発など対応していました。
+                                 アルバイトでWebコーダ、フロントエンドエンジニアとして参加しました。<br/>
+                                 Wordpress案件やECショップの立ち上げ、軽い予約システムの開発など対応していました。
                              </>}
                              icons={[logotype.WORDPRESS, logotype.HTML, logotype.CSS, logotype.JAVASCRIPT]}/>
             </div>
